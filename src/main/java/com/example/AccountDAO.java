8f731363668f6301ccbe67b1d599c652a5c17228
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-03 10:58:27 +0900

    s

--- src/main/java/com/example/AccountDAO.java
+++ src/main/java/com/example/AccountDAO.java
@@ -1,18 +1,40 @@
-/*package com.example;
+SpringBootEncodeApplication.java
+package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+@SpringBootApplication
+public class SpringBootEncodeApplication implements CommandLineRunner {
 
-public class AccountDAO{
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-	public void insertAccount(String custid,String username,String orgname,String password){
-		Connection con = null;
-		PreparedStatement ps =null;
-		ResultSet rs =null;
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEncodeApplication.class, args);
+    }
 
-	}
-}*/
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void run(String... args) {
+
+        String password = "#fe?3d31";
+        String digest = passwordEncoder.encode(password);
+        System.out.println("ハッシュ値 = " + digest);
+
+        if (passwordEncoder.matches(password, digest)) {
+            System.out.println("一致したよ");
+            return;
+        }
+        System.out.println("一致しなかったよ");
+    }
+}
