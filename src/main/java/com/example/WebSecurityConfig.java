10e02c59b8d372f206ef610703a7cc6df7eea5fc
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-03 16:03:25 +0900

    d

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -1,33 +1,34 @@
 /*package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+package test.model;
 
-import javax.sql.DataSource;
+import lombok.Data;
 
-@Configuration
-@EnableWebMvcSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/signup").permitAll().anyRequest().authenticated();
+@Entity
+@Table(name = "accounts")
+@Data
+public class Account implements Serializable {
 
-    }
+    @Column(nullable=false, length=20)
+    private String custid;
 
-    @Configuration
-    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
-        @Autowired
-        private DataSource dataSource;
+    @Column(nullable=false, length=20)
+    private String custname;
 
-        @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception {
-            auth.jdbcAuthentication().dataSource(dataSource);
-        }
-    }
-}*/
+     @Column(nullable=false, length=20)
+    private String orgname;
+
+    @Column(nullable=false, length=20)
+    private String password;
+
+    @Column(nullable=false, length=20)
+    private String role;
+
+    @Column(nullable=false, length=20)
+    private String reserve;
+}
+*/
