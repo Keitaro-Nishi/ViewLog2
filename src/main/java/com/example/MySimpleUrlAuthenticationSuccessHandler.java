601a26d1cd6c8d1d38ee5ddead8d04102dfe0645
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-05 15:42:03 +0900

    d

--- src/main/java/com/example/MySimpleUrlAuthenticationSuccessHandler.java
+++ src/main/java/com/example/MySimpleUrlAuthenticationSuccessHandler.java
@@ -1,3 +1,4 @@
+/*
 package com.example;
 
 import java.io.IOException;
@@ -68,12 +69,12 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
             throw new IllegalStateException();
         }
     }
-
+*/
     /**
      * Removes temporary authentication-related data which may have been stored in the session
      * during the authentication process.
      */
-
+/*
     protected final void clearAuthenticationAttributes(final HttpServletRequest request) {
         final HttpSession session = request.getSession(false);
 
@@ -93,3 +94,4 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
     }
 
 }
+*/
