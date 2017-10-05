099e9e30b77deb149c2d724b3d4ad59e23c7d095
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-05 09:39:38 +0900

    d

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		//.antMatchers("/User/**").hasAuthority("ADMIN")
 		.and()
 		.formLogin()
-		.loginPage("/home.jsp");
+		.loginPage("/login");
 		http.formLogin()
 		.defaultSuccessUrl("/Home", true)
 		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
