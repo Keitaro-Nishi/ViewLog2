987b233f927e02b5aca49f73e56cd2b611b372d3
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-10 15:31:21 +0900

    g

--- src/main/java/com/example/MenuController.java
+++ src/main/java/com/example/MenuController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.LoginUser;
+//import com.example.LoginUser;
 
 @Controller
 public class MenuController {
