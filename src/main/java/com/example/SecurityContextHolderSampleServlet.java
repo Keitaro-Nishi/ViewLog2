1fe84136c0549f4ad07252ac5cddd9cd52e7c31f
sakazawasaburo <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-11 13:50:54 +0900

    s

---
+++ src/main/java/com/example/SecurityContextHolderSampleServlet.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static java.util.stream.Collectors.*;
+
+@WebServlet("/authentication")
+public class SecurityContextHolderSampleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("[authorities]");
+        System.out.println("  " + auth.getAuthorities().stream()
+                                    .map(GrantedAuthority::getAuthority)
+                                    .collect(joining("\n  ")));
+
+        WebAuthenticationDetails details = (WebAuthenticationDetails) auth.getDetails();
+        System.out.println("[details]");
+        System.out.println("  IP Address : " + details.getRemoteAddress());
+        System.out.println("  Session ID : " + details.getSessionId());
+
+        UserDetails principal = (UserDetails) auth.getPrincipal();
+        System.out.println("[principal]");
+        System.out.println("  username : " + principal.getUsername());
+        System.out.println("  password : " + principal.getPassword());
+
+        System.out.println("[credentials]");
+        System.out.println("  " + auth.getCredentials());
+    }
+}
