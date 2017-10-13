131a072b04a992369b588c0e6b45df0f100cd0d0
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-13 10:49:17 +0900

    s

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/login").permitAll()
 		.antMatchers("/").hasAnyAuthority("ADMIN","USER")
 		.antMatchers("/Home").hasAnyAuthority("ADMIN","USER")
-		.antMatchers("/Account").hasAuthority("ADMIN")
+		.antMatchers("/Account/**").hasAuthority("ADMIN")
 		.antMatchers("/logout").hasAnyAuthority("ADMIN","USER")
 		//.antMatchers("/User/**").hasAuthority("ADMIN")
 		.and()
