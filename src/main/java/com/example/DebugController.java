dca7ac0a442080769dcbc6f83b2e093413756da2
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-18 10:20:10 +0900

    s

--- src/main/java/com/example/DebugController.java
+++ src/main/java/com/example/DebugController.java
@@ -1,18 +1,55 @@
-/*
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.authentication.encoding.BasePasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
 
+public UserDetails getLoggedInUser(){
 
-@Controller
-@Slf4j
-public class DebugController {
-
-    @RequestMapping({"/debug"})
-    public String debug(@AuthenticationPrincipal AppUserDetail userDetail) {
-        log.info("debug: {}", userDetail);
-
-        return "user";
-    }
+	final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	if (auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken))
+	{
+		if(auth.getDetails() !=null)
+			System.out.println(auth.getDetails().getClass());
+		if( auth.getDetails() instanceof UserDetails)
+		{
+			System.out.println("UserDetails");
+		}
+		else
+		{
+			System.out.println("!UserDetails");
+		}
+	}
+	return null;
 }
-*/
