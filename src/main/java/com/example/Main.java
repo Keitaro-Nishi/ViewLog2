ac923ec9e9f1d71c35d5be5b8adbabac9f79842c
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-20 13:53:53 +0900

    s

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -45,6 +45,12 @@ import java.util.Map;
 @SpringBootApplication
 public class Main {
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
 	}
@@ -84,26 +90,36 @@ public class Main {
 		return "home";
 	}
 
-	  @RequestMapping("/index")
-	  String db(Map<String, Object> model) {
-	    try (Connection connection = dataSource.getConnection()) {
-	      Statement stmt = connection.createStatement();
-	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+	@RequestMapping("/index")
+	String db(Map<String, Object> model) {
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-	      ArrayList<String> output = new ArrayList<String>();
-	      while (rs.next()) {
-	        output.add("Read from DB: " + rs.getTimestamp("tick"));
-	      }
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+				output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
 
-	      model.put("records", output);
-	      return "index";
-	    } catch (Exception e) {
-	      model.put("message", e.getMessage());
-	      return "error";
-	    }
-	  }
+			model.put("records", output);
+			return "index";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
 
 /*  Userクラス  */
 /*
@@ -113,5 +129,5 @@ public class Main {
         modelMap.addAttribute("index", user);
         return "index";
     }
-*/
+ */
 }
