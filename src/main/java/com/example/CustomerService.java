1125c63e884e9356fdf7b468b021df96005b0360
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-25 13:45:04 +0900

    s

--- src/main/java/com/example/CustomerService.java
+++ src/main/java/com/example/CustomerService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import src.main.java.com.example.Customer;
+import ViewLog.src.main.java.com.example.Customer;
 import src.main.java.com.example.CustomerRepository;
 
 @Service    // a
