21fa2ca41657575d7f6e11317cd4898a2c3652cb
sakazawasaburo <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-25 13:27:34 +0900

    s

---
+++ src/main/java/com/example/CustomerService.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tools.springsample.springsample04.domain.Customer;
+import tools.springsample.springsample04.repository.CustomerRepository;
+
+@Service    // a
+@Transactional    // b
+public class CustomerService {
+    @Autowired    // c
+    CustomerRepository repository;    // d
+
+    // 顧客全件取得
+    public List<Customer> findAll() {    // e
+        return repository.findAll();
+    }
+
+    // 顧客一件取得
+    public Customer findOne(Integer id) {    // f
+        return repository.findOne(id);
+    }
+
+    // 顧客一件作成
+    public Customer create(Customer customer) {    // g
+        return repository.save(customer);
+    }
+
+    // 顧客一件更新
+    public Customer update(Customer customer) {    // h
+        return repository.save(customer);
+    }
+
+    // 顧客一件削除
+    public void delete(Integer id) {    // i
+        repository.delete(id);
+    }
+}
