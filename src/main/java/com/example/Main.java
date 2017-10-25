c855bd15a9c457acf1b4ad8b6ebe3bae4be208bc
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-25 14:13:52 +0900

    s

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -117,6 +117,7 @@ public class Main {
 		}
 	}
 */
+	/*
 	@RequestMapping("/Account")
 	String db(Map<String, Object> model) {
 		try (Connection connection = dataSource.getConnection()) {
@@ -137,7 +138,7 @@ public class Main {
 			return "error";
 		}
 	}
-
+*/
 	@Bean
 	@ConfigurationProperties("spring.datasource")
 	public DataSource dataSource() throws SQLException {
