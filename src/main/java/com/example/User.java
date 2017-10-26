b2e6f66a4980fd0262934f669485c975c0d7b6de
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-26 10:11:12 +0900

    ｓ

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,39 +1,14 @@
-/*
 package com.example;
 
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@Entity    // a
-@Table(name="userdata")    // b
-@Data    // c
-@AllArgsConstructor    // d
-@NoArgsConstructor    // e
+@Data    // a
+@AllArgsConstructor  // b
+@NoArgsConstructor  // c
 public class Customer {
-
-    @Id
-    @GeneratedValue
-    private Integer no;
-
-    @Column(nullable=false)
-    private String custid;
-
-    @Column(nullable=false)
-    private String custname;
-
-    @Column(nullable=false)
-    private String orgname;
-
-    @Column(nullable=false)
-    private String reserve;
+    private Integer id;
+    private String name;
+    private String email;
 }
-*/
