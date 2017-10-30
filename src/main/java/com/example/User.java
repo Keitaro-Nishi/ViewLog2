2f5fec2252e8ae20a14c3a8dfcec0fbfe0fed7a8
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-10-30 19:59:02 +0900

    s

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,19 +1,40 @@
-/*
 package com.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="userdata")
 @Data    // a
 @AllArgsConstructor  // b
-@NoArgsConstructor  // c
+@NoArgsConstructor // c
 public class User {
-	private Integer no;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int no;
+
+	@Column(name="custid")
 	private String custid;
+
+	@Column(name="custname")
 	private String custname;
+
+	@Column(name="orgname")
 	private String orgname;
+
+	@Column(name="role")
 	private String role;
+
+	@Column(name="reserve")
 	private String reserve;
+
 }
-*/
