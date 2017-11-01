54f38dedf776fbabcda5e222e3b13a58b6e4359a
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-11-01 09:53:07 +0900

    s

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -1,35 +1,19 @@
 package com.example;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.example.User;
-import com.example.UserRepository;
+@Service
+public class UserService implements UserDetailsService {
 
-@Service    // a
-@Transactional    // b
-public class UserService implements UserDetailsService{
+	@Autowired
+	private UserRepository repository;
 
-	@Override
-	public UserDetails loadUserByUsername(String username)
-			throws UsernameNotFoundException {
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-		@Autowired    // c
-		private UserRepository repository;    // d
-
-		@Autowired
-		private PasswordEncoder passwordEncoder;
-	}
-	// 顧客全件取得
-	public List<User> findAll() {    // e
-		return repository.findAll();
-
-	}
 }
