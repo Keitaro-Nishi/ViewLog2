79496570464a0b59eb822a86e0924f3ab84c5be2
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-11-01 09:48:30 +0900

    s

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -26,10 +26,10 @@ public class UserService implements UserDetailsService{
 
 		@Autowired
 		private PasswordEncoder passwordEncoder;
+	}
+	// 顧客全件取得
+	public List<User> findAll() {    // e
+		return repository.findAll();
 
-		// 顧客全件取得
-		public List<User> findAll() {    // e
-			return repository.findAll();
-		}
 	}
 }
