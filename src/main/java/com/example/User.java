841b01144af2df840afc8e117f10f81b1d6bad36
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-11-01 09:26:34 +0900

    s

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -36,11 +36,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor  // b
 @NoArgsConstructor // c
 public class User implements UserDetails{
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public enum Authority {USER, ADMIN};
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int no;
@@ -56,23 +56,79 @@ public class User implements UserDetails{
 
 	@Column(name="password")
 	private String password;
-	
+
 	@Column(name="role")
 	private String role;
 
 	@Column(name="reserve")
 	private String reserve;
 
-    // JPA requirement
-    protected User() {}
-    
-    public User(String custid, String custname, String orgname, String password, String role, String reserve) {
-        this.custid = custid;
-    	this.custname = custname;
-    	this.orgname = orgname;
-        this.password = password;
-        this.role = role;
-        this.reserve = reserve;
-    }
-    
+	// JPA requirement
+	protected User() {}
+
+	public User(String custid, String custname, String orgname, String password, String role, String reserve) {
+		this.custid = custid;
+		this.custname = custname;
+		this.orgname = orgname;
+		this.password = password;
+		this.role = role;
+		this.reserve = reserve;
+	}
+
+	//@Override
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	//@Override
+	public String getCustid() {
+		return custid;
+	}
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+
+	//@Override
+	public String getCustname() {
+		return custname;
+	}
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+
+	//@Override
+	public String getOrgname() {
+		return orgname;
+	}
+	public void setOrgname(String orgname) {
+		this.orgname = orgname;
+	}
+
+	//@Override
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	//@Override
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	//@Override
+	public String getReserve() {
+		return role;
+	}
+	public void setReserve(String reserve) {
+		this.reserve = reserve;
+	}
+
 }
