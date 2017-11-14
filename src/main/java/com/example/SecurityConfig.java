66e34bed1891df413ea17a68a654b984aa5ac38b
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-11-14 14:23:27 +0900

    s

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -1,23 +1,45 @@
-package viewlog;
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
 @SpringBootApplication
-public class SecurityConfig {
+public class Main {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SecurityConfig.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 	@RequestMapping({"/","/login"})
